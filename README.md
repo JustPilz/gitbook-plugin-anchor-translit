854582c5ff3245448839f09b5115d61ba010d23e
JustPilz <dkrivyakov@gmail.com>
2017-06-19 21:42:00 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,16 @@
+# Auto translit \#anchor for headings
+### Install
+Run:
+
+```
+    npm i gitbook-plugin-anchor-translit
+```
+
+Or add the below to your `book.json` file, then run `gitbook install`:
+
+```json
+{
+    "plugins": ["anchor-translit"]
+}
+```
+That's all!
---
+++ index.js
@@ -0,0 +1,22 @@
+﻿var slug = require('github-slugid');
+var translit = require('translit')({
+  'А': 'A', 'а': 'a', 'Б': 'B', 'б': 'b', 'В': 'V', 'в': 'v', 'Г': 'G', 'г': 'g', 'Д': 'D', 'д': 'd',
+  'Е': 'E', 'е': 'e', 'Ё': 'E', 'ё': 'e', 'Ж': 'Zh', 'ж': 'zh', 'З': 'Z', 'з': 'z', 'И': 'I', 'и': 'i',
+  'Й': 'Y', 'й': 'y', 'К': 'K', 'к': 'k', 'Л': 'L', 'л': 'l', 'М': 'M', 'м': 'm', 'Н': 'N', 'н': 'n',
+  'О': 'O', 'о': 'o', 'П': 'P', 'п': 'p', 'Р': 'R', 'р': 'r', 'С': 'S', 'с': 's', 'Т': 'T', 'т': 't',
+  'У': 'U', 'у': 'u', 'Ф': 'F', 'ф': 'f', 'Х': 'Kh', 'х': 'kh', 'Ц': 'Ts', 'ц': 'ts', 'Ч': 'Ch',
+  'ч': 'ch', 'Ш': 'Sh', 'ш': 'sh', 'Щ': 'Sch', 'щ': 'sch', 'Ь': '', 'ь': '', 'Ы': 'Y', 'ы': 'y',
+  'Ъ': '', 'ъ': '', 'Э': 'E', 'э': 'e', 'Ю': 'Yu', 'ю': 'yu', 'Я': 'Ya', 'я': 'ya'
+});
+
+module.exports = {
+    hooks: {
+        "page:before": function (page) {
+            page.content = page.content.replace(/(#{1,6}\s(.*?[^\}|\r]))$/mg, function(str, p1, p2, offset, s) {
+                 if(p2) return p1+' {#'+slug(translit(p2))+'}';
+                 return s;
+            });
+            return page;
+        }
+    }
+};
